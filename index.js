12090697968f37809ea403fc8acb66f9ce304403
Himalaya vats <contact@himalayavats.com>
2021-10-01 23:42:03 +0530

    Edit some cors policy

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const upload = require('./routes/upload');
 
 const app = express();
 app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    "origin": "*"
   }));
 
 app.use(express.json())
